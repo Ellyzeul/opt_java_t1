829e47a12464fa9b7bc8af97951f78baf18d846a
yingyangtongxue <daniel.n.monteiro@unesp.br>
2022-06-19 23:47:14 -0300

    feat: início model

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,5 +1,25 @@
 package src.model;
 
-public class Car {
+public class Car extends Thread {
+    private int totalLaps; 
+    private float probCrash; 
+    private float probRefuel; 
+    private String text;
+
+    public Car(int totalLaps, float probCrash, float probRefuel, String text){
+        this.totalLaps = totalLaps; 
+        this.probCrash = probCrash; 
+        this.probRefuel = probRefuel; 
+        this.text = text;
+    }
   
+    @Override
+    public void run(){
+        syncronized(text){ //error
+            for(int i=1; i<=10;i++){
+                text = text +i+" hello\n";
+                System.out.println(text);
+            }            
+        }
+    }
 }
